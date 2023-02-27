8ccd1982d953b5521a58912899bbbe19c0e41083
lcastiglione <leonardocastiglione@hotmail.com>
2023-02-27 14:46:12 -0300

    Abstract Either type

---
+++ packages/core/index.ts
@@ -0,0 +1,4 @@
+﻿export * from './src/domain/common/Either';
+export * from './src/domain/common/EitherAsync';
+export * from './src/domain/common/ValueObject';
+export * from './src/presentation/common/Bloc';
