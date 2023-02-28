d9bd4d1b55d547102af1c628adde5e751dca8593
lcastiglione <leonardocastiglione@hotmail.com>
2023-02-28 08:12:37 -0300

    refactor create and equals method

--- packages/core/src/domain/common/ValueObject.ts
+++ packages/core/src/domain/common/ValueObject.ts
@@ -1,8 +1,21 @@
-﻿interface ValueObjectProps {
+﻿/**
+ * Interfaz que define un índice de propiedad genérico para el la clase `ValueObject`.
+ */
+interface ValueObjectProps {
   [index: string]: any
 }
 
+/**
+ * Clase que define un `ValueObject` genérico que puede ser extendido para implementar objetos específicos.
+ *
+ * @param T - El tipo de las propiedades del objeto de valor.
+ */
 export abstract class ValueObject<T extends ValueObjectProps> {
+  /**
+   * Crea una nueva instancia de un `ValueObject`.
+   *
+   * @param props - Las propiedades del `ValueObject`.
+   */
   constructor(protected props: T) {
     const baseProps: any = {
       ...props,
@@ -10,20 +23,35 @@ export abstract class ValueObject<T extends ValueObjectProps> {
     this.props = baseProps
   }
 
-  protected static create<T extends ValueObjectProps>(this: new (props: T) => ValueObject<T>, props: T): ValueObject<T> {
-    return new this(props)
+  /**
+   * Crea una nueva instancia de un `ValueObject`.
+   *
+   * @param props - Las propiedades del `ValueObject`.
+   * @returns Una nueva instancia de un `ValueObject`.
+   * @throws Un error si una subclase no implementa `create`.
+   */
+  public static create(props: ValueObjectProps): ValueObject<ValueObjectProps> {
+    throw new Error('create not defined')
   }
 
+  /**
+   * Determina si este `ValueObject` es igual a otro.
+   *
+   * @param vo - El `ValueObject` a comparar.
+   * @returns `true` si los value objects son iguales; de lo contrario, `false`.
+   */
   public equals(vo?: ValueObject<T>): boolean {
-    if (vo === null || vo === undefined) {
-      return false
+    if (!vo || !(vo instanceof ValueObject)) {
+      return false;
     }
-    if (vo.props === undefined) {
-      return false
-    }
-    return JSON.stringify(this.props) === JSON.stringify(vo.props)
+    return JSON.stringify(this.props) === JSON.stringify(vo.props);
   }
 
+  /**
+   * Crea una copia de este `ValueObject`.
+   *
+   * @returns Un nuevo `ValueObject` que es una copia de este `ValueObject`.
+   */
   public clone(): ValueObject<T> {
     const clonedProps: T = JSON.parse(JSON.stringify(this.props))
     return new (this.constructor as any)(clonedProps)
