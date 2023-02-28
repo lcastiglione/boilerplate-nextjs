6efe4dce92a7e9814f9f3ec7dc2dbd1049a3ae38
lcastiglione <leonardocastiglione@hotmail.com>
2023-02-28 14:21:12 -0300

    Code fixed

--- packages/core/src/domain/common/__tests__/Either.test.ts
+++ packages/core/src/domain/common/__tests__/Either.test.ts
@@ -1,136 +1,138 @@
-﻿import { Either } from '../Either'
+﻿import { Either } from '../Either';
 
 export interface ApiFailure {
-  kind: 'ApiFailure'
-  statusCode: number
+  kind: 'ApiFailure';
+  statusCode: number;
 }
 
 export interface UnexpectedFailure {
-  kind: 'UnexpectedFailure'
-  error: Error
+  kind: 'UnexpectedFailure';
+  error: Error;
 }
 
 export interface AnonymousUserFailure {
-  kind: 'AnonymousUserFailure'
+  kind: 'AnonymousUserFailure';
 }
 
-type ProcessFailure = ApiFailure | UnexpectedFailure | AnonymousUserFailure
+type ProcessFailure = ApiFailure | UnexpectedFailure | AnonymousUserFailure;
 
 function createApiFailure(code: number): Either<ApiFailure, number> {
-  return Either.left({ kind: 'ApiFailure', statusCode: code })
+  return Either.left({ kind: 'ApiFailure', statusCode: code });
 }
 
 describe('Either Class', () => {
   describe('isLeft and isRight', () => {
     it('should return isLeft equal to true for a left value', () => {
-      const result = Either.left({ kind: 'error' })
+      const result = Either.left({ kind: 'error' });
 
-      expect(result.isLeft()).toBeTruthy()
-      expect(result.isRight()).toBeFalsy()
-    })
+      expect(result.isLeft()).toBeTruthy();
+      expect(result.isRight()).toBeFalsy();
+    });
 
     it('should return isRight equal to true for a right value', () => {
-      const result = Either.right(5)
+      const result = Either.right(5);
 
-      expect(result.isRight()).toBeTruthy()
-      expect(result.isLeft()).toBeFalsy()
-    })
-  })
+      expect(result.isRight()).toBeTruthy();
+      expect(result.isLeft()).toBeFalsy();
+    });
+  });
 
   describe('fold', () => {
     it('should return expected left for a left value', () => {
-      const result = Either.left({ kind: 'error1' })
+      const result = Either.left({ kind: 'error1' });
 
       result.fold(
         (error) => expect(error.kind).toEqual('error1'),
         () => fail('should be error')
-      )
-    })
+      );
+    });
 
     it('should return expected right for a right value', () => {
-      const result = Either.right(5)
+      const result = Either.right(5);
 
       result.fold(
         (error) => fail(error),
         (value) => expect(value).toEqual(5)
-      )
-    })
-  })
+      );
+    });
+  });
 
   describe('getOrElse', () => {
     it('should getOrElse return default value for left', () => {
-      const result = Either.left({ kind: 'error1' })
+      const result = Either.left({ kind: 'error1' });
 
-      expect(result.getOrElse(0)).toEqual(0)
-    })
+      expect(result.getOrElse(0)).toEqual(0);
+    });
 
     it('should getOrElse return a right value for right', () => {
-      const result = Either.right(5)
+      const result = Either.right(5);
 
-      expect(result.getOrElse(0)).toEqual(5)
-    })
-  })
+      expect(result.getOrElse(0)).toEqual(5);
+    });
+  });
 
   describe('map', () => {
     it('should return fail mapped value for a left initial value', () => {
-      const result: Either<ProcessFailure, number> = createApiFailure(404)
-      const mappedResult = result.map((value) => value * 2)
+      const result: Either<ProcessFailure, number> = createApiFailure(404);
+      const mappedResult = result.map((value) => value * 2);
 
       mappedResult.fold(
         (error) => expect(error.kind).toEqual('ApiFailure'),
         () => fail('should be error')
-      )
-    })
+      );
+    });
 
     it('should return success mapped value for a initial valid value', () => {
-      const result = Either.right(5)
-      const mappedResult = result.map((value) => value * 2)
+      const result = Either.right(5);
+      const mappedResult = result.map((value) => value * 2);
 
       mappedResult.fold(
         (error) => fail(error),
         (value) => expect(value).toEqual(10)
-      )
-    })
-  })
+      );
+    });
+  });
 
   describe('flatmap', () => {
     it('should return first error after flatmap for a left initial value', () => {
-      const result: Either<ProcessFailure, number> = createApiFailure(404)
-      const mappedResult = result.flatMap((value) => Either.right(value * 2))
+      const result: Either<ProcessFailure, number> = createApiFailure(404);
+      const mappedResult = result.flatMap((value) => Either.right(value * 2));
 
       mappedResult.fold(
         (error) => expect(error.kind).toEqual('ApiFailure'),
         () => fail('should be error')
-      )
-    })
+      );
+    });
 
     it('should return success mapped value after flatmap for a initial valid value', () => {
-      const result = Either.right(5)
-      const mappedResult = result.flatMap((value) => Either.right(value * 2))
+      const result = Either.right(5);
+      const mappedResult = result.flatMap((value) => Either.right(value * 2));
 
       mappedResult.fold(
         (error) => fail(error),
         (value) => expect(value).toEqual(10)
-      )
-    })
-  })
+      );
+    });
+  });
 
   describe('getOrThrow', () => {
     it('should throw an error when called on a left value', () => {
-      const leftValue = Either.left('error message')
-      expect(() => leftValue.getOrThrow()).toThrow('An error has ocurred: {"kind":"left","leftValue":"error message"}')
-    })
+      const leftValue = Either.left('error message');
+      expect(() => leftValue.getOrThrow()).toThrow(
+        'An error has ocurred: {"kind":"left","leftValue":"error message"}'
+      );
+    });
 
     it('should return the right value when called on a right value', () => {
-      const rightValue = Either.right(42)
-      expect(rightValue.getOrThrow()).toEqual(42)
-    })
+      const rightValue = Either.right(42);
+      expect(rightValue.getOrThrow()).toEqual(42);
+    });
 
     it('should throw a custom error message when one is provided and called on a left value', () => {
-      const leftValue = Either.left('error message')
+      const leftValue = Either.left('error message');
       expect(() => leftValue.getOrThrow('custom error message')).toThrow(
         'custom error message'
-      )
-    })
-  })
-})
+      );
+    });
+  });
+});
--- packages/core/src/domain/common/__tests__/ValueObject.test.ts
+++ packages/core/src/domain/common/__tests__/ValueObject.test.ts
@@ -1,77 +1,75 @@
-﻿import { ValueObject } from '../ValueObject'
+﻿import { ValueObject } from '../ValueObject';
 
 interface PersonProps {
-  firstName: string
-  lastName: string
+  firstName: string;
+  lastName: string;
 }
 
 export class Person extends ValueObject<PersonProps> {
   constructor(props: PersonProps) {
-    super(props)
+    super(props);
   }
 
   get firstName(): string {
-    return this.props.firstName
+    return this.props.firstName;
   }
 
   get lastName(): string {
-    return this.props.lastName
+    return this.props.lastName;
   }
 
   public static create(props: PersonProps): Person {
-    return new Person(props)
+    return new Person(props);
   }
 }
 
 export class PersonEmpty extends ValueObject<PersonProps> {
-    constructor(props: PersonProps) {
-      super(props)
-    }
+  constructor(props: PersonProps) {
+    super(props);
   }
+}
 
 describe('ValueObject Class', () => {
   describe('equals', () => {
     it('should return false if the given object is empty', () => {
-      const valueObject = new Person({ firstName: 'John', lastName: 'Doe' })
-      expect(valueObject.equals()).toBe(false)
-    })
+      const valueObject = new Person({ firstName: 'John', lastName: 'Doe' });
+      expect(valueObject.equals()).toBe(false);
+    });
 
     it('should return true if the names have the same values', () => {
-      const name1 = new Person({ firstName: 'John', lastName: 'Doe' })
-      const name2 = new Person({ firstName: 'John', lastName: 'Doe' })
-      expect(name1.equals(name2)).toBe(true)
-    })
+      const name1 = new Person({ firstName: 'John', lastName: 'Doe' });
+      const name2 = new Person({ firstName: 'John', lastName: 'Doe' });
+      expect(name1.equals(name2)).toBe(true);
+    });
 
     it('should return false if the names have different values', () => {
-      const name1 = new Person({ firstName: 'John', lastName: 'Doe' })
-      const name2 = new Person({ firstName: 'Jane', lastName: 'Doe' })
-      expect(name1.equals(name2)).toBe(false)
-    })
-  })
+      const name1 = new Person({ firstName: 'John', lastName: 'Doe' });
+      const name2 = new Person({ firstName: 'Jane', lastName: 'Doe' });
+      expect(name1.equals(name2)).toBe(false);
+    });
+  });
 
   describe('clone', () => {
     it('should return a new instance with the same properties', () => {
-      const name1 = new Person({ firstName: 'John', lastName: 'Doe' })
-      const name2 = name1.clone()
-      expect(name1).toEqual(name2)
-      expect(name1).not.toBe(name2)
-    })
-  })
+      const name1 = new Person({ firstName: 'John', lastName: 'Doe' });
+      const name2 = name1.clone();
+      expect(name1).toEqual(name2);
+      expect(name1).not.toBe(name2);
+    });
+  });
 
   describe('create', () => {
     it('should create a valid instance of PersonName', () => {
-      const nameProps = { firstName: 'John', lastName: 'Doe' }
-      const name = Person.create(nameProps)
-      expect(name).toBeInstanceOf(Person)
-      expect(name.firstName).toBe('John')
-      expect(name.lastName).toBe('Doe')
-    })
+      const nameProps = { firstName: 'John', lastName: 'Doe' };
+      const name = Person.create(nameProps);
+      expect(name).toBeInstanceOf(Person);
+      expect(name.firstName).toBe('John');
+      expect(name.lastName).toBe('Doe');
+    });
 
     it('should return error by create not defined', () => {
-        const nameProps = { firstName: 'John', lastName: 'Doe' }
-        expect(() => PersonEmpty.create(nameProps)).toThrow(
-            'create not defined'
-          )
-      })
-  })
-})
+      const nameProps = { firstName: 'John', lastName: 'Doe' };
+      expect(() => PersonEmpty.create(nameProps)).toThrow('create not defined');
+    });
+  });
+});
