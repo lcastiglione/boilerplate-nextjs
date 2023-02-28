e4989163e30dd32d3a31b41e565c326f0fd2092b
Leo <leonardocastiglione@hotmail.com>
2023-02-27 22:02:07 -0300

    Template unit test ValueObject

---
+++ packages/core/src/domain/common/__tests__/ValueObject.test.ts
@@ -0,0 +1,11 @@
+﻿export {}
+
+describe("ValueObject class unit testing", () => {
+    it('should return true if equal value objects', () => {
+
+    })
+
+    it('should return clone object with different reference', () => {
+
+    })
+});
