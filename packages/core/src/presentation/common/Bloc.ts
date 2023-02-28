fd93f69724655b63e3d1775ec8c3acbb71d12d00
lcastiglione <leonardocastiglione@hotmail.com>
2023-02-28 14:22:06 -0300

    Add comments

--- packages/core/src/presentation/common/Bloc.ts
+++ packages/core/src/presentation/common/Bloc.ts
@@ -1,17 +1,40 @@
-﻿type Subscription<S> = (state: S) => void
+﻿/**
+ * La interfaz `Subscription` describe una función que recibe el estado actual como argumento.
+ *
+ * @template S El tipo del estado.
+ */
+type Subscription<S> = (state: S) => void
 
+/**
+ * La clase abstracta `Bloc` proporciona una implementación básica de la arquitectura Bloc (Business Logic Component).
+ *
+ * @template S El tipo del estado del Bloc.
+ */
 export abstract class Bloc<S> {
   private internalState: S
   private listeners: Subscription<S>[] = []
 
-  constructor(initalState: S) {
-    this.internalState = initalState
+  /**
+   * Crea una nueva instancia de Bloc con el estado inicial especificado.
+   *
+   * @param initialState El estado inicial del Bloc.
+   */
+  constructor(initialState: S) {
+    this.internalState = initialState
   }
 
+  /**
+   * Obtiene el estado actual del Bloc.
+   */
   public get state(): S {
     return this.internalState
   }
 
+  /**
+   * Cambia el estado interno del Bloc y notifica a todos los suscriptores.
+   *
+   * @param state El nuevo estado del Bloc.
+   */
   changeState(state: S) {
     this.internalState = state
 
@@ -20,10 +43,20 @@ export abstract class Bloc<S> {
     }
   }
 
+  /**
+   * Suscribe una función al Bloc para que sea notificada cada vez que cambie su estado.
+   *
+   * @param listener La función que se suscribirá al Bloc.
+   */
   subscribe(listener: Subscription<S>) {
     this.listeners.push(listener)
   }
 
+  /**
+   * Cancela la suscripción de una función al Bloc.
+   *
+   * @param listener La función que se cancelará la suscripción.
+   */
   unsubscribe(listener: Subscription<S>) {
     const index = this.listeners.indexOf(listener)
     if (index > -1) {
