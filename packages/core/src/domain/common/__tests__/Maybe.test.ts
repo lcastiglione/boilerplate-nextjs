12a2c5e0a2f5e4b89580b7480526b9b3ad32ab92
Leo <leonardocastiglione@hotmail.com>
2023-03-01 20:30:57 -0300

    Add unit tests Maybe class

---
+++ packages/core/src/domain/common/__tests__/Maybe.test.ts
@@ -0,0 +1,147 @@
+﻿import { Maybe } from '../Maybe';
+
+describe('Maybe Class', () => {
+  describe('isDefined and isEmpty', () => {
+    it('should return isEmpty equal to true for a undefined value', () => {
+      const maybe = Maybe.fromValue(undefined);
+
+      expect(maybe.isDefined()).toBeFalsy();
+      expect(maybe.isEmpty()).toBeTruthy();
+    });
+    it('should return isEmpty equal to true for a null value', () => {
+      const maybe = Maybe.fromValue(null);
+
+      expect(maybe.isDefined()).toBeFalsy();
+      expect(maybe.isEmpty()).toBeTruthy();
+    });
+    it('should return isDefined equal to true for a valid value', () => {
+      const maybe = Maybe.fromValue(5);
+
+      expect(maybe.isDefined()).toBeTruthy();
+      expect(maybe.isEmpty()).toBeFalsy();
+    });
+  });
+
+  describe('fold', () => {
+    it('should return none for a undefined value', () => {
+      const maybe = Maybe.fromValue(undefined);
+
+      maybe.fold(
+        () => expect(maybe.isEmpty()).toBeTruthy(),
+        () => fail('Should return none for a undefined value')
+      );
+    });
+    it('should return none for a null value', () => {
+      const maybe = Maybe.fromValue(null);
+
+      maybe.fold(
+        () => expect(maybe.isEmpty()).toBeTruthy(),
+        () => fail('Should return none for a null value')
+      );
+    });
+    it('should return some for a valid value', () => {
+      const maybe = Maybe.fromValue(5);
+
+      maybe.fold(
+        () => fail('Should return expected mapped value after flatmap'),
+        (value) => expect(value).toEqual(5)
+      );
+    });
+
+    describe('get', () => {
+      it('should get throw an error for undefined value', () => {
+        const maybe = Maybe.fromValue(undefined);
+
+        expect(maybe.get).toThrow();
+      });
+      it('should get throw an error for null value', () => {
+        const maybe = Maybe.fromValue(null);
+
+        expect(maybe.get).toThrow();
+      });
+      it('should get return the valid value', () => {
+        const maybe = Maybe.fromValue(5);
+
+        expect(maybe.get()).toEqual(5);
+      });
+    });
+  });
+
+  describe('getOrElse', () => {
+    it('should getOrElse return default value for undefined value', () => {
+      const maybe = Maybe.fromValue<number>(undefined);
+
+      expect(maybe.getOrElse(0)).toEqual(0);
+    });
+    it('should getOrElse return default value for null value', () => {
+      const maybe = Maybe.fromValue<number>(null);
+
+      expect(maybe.getOrElse(0)).toEqual(0);
+    });
+    it('should getOrElse return a value for valid value', () => {
+      const maybe = Maybe.fromValue(5);
+
+      expect(maybe.getOrElse(0)).toEqual(5);
+    });
+  });
+
+  describe('map', () => {
+    it('should return none after map a undefined value', () => {
+      const maybe = Maybe.fromValue<number>(undefined);
+      const mappedMaybe = maybe.map((number) => number * 2);
+
+      mappedMaybe.fold(
+        () => expect(mappedMaybe.isEmpty()).toBeTruthy(),
+        () => fail('Should return none after map a undefined value')
+      );
+    });
+    it('should return none after map a null value', () => {
+      const maybe = Maybe.fromValue<number>(null);
+      const mappedMaybe = maybe.map((number) => number * 2);
+
+      mappedMaybe.fold(
+        () => expect(mappedMaybe.isEmpty()).toBeTruthy(),
+        () => fail('Should return none after map a null value')
+      );
+    });
+    it('should return some after map a valid value', () => {
+      const maybe = Maybe.fromValue(5);
+      const mappedMaybe = maybe.map((number) => number * 2);
+
+      mappedMaybe.fold(
+        () => fail('Should return expected mapped value after flatmap'),
+        (value) => expect(value).toEqual(10)
+      );
+    });
+  });
+
+  describe('flatMap', () => {
+    it('should return none after flatmap a null initial value', () => {
+      const maybe = Maybe.fromValue<number>(undefined);
+      const mappedMaybe = maybe.flatMap((value) => Maybe.fromValue(value * 2));
+
+      mappedMaybe.fold(
+        () => expect(mappedMaybe.isEmpty()).toBeTruthy(),
+        () => fail('Should return none after map a null value')
+      );
+    });
+    it('should return initial none after flatmap for a null initial value', () => {
+      const maybe = Maybe.fromValue<number>(null);
+      const mappedMaybe = maybe.flatMap((value) => Maybe.fromValue(value * 2));
+
+      mappedMaybe.fold(
+        () => expect(mappedMaybe.isEmpty()).toBeTruthy(),
+        () => fail('Should return none after map a null value')
+      );
+    });
+    it('should return expected mapped value after flatmap for a initial valid value', () => {
+      const maybe = Maybe.fromValue(5);
+      const mappedMaybe = maybe.flatMap((value) => Maybe.fromValue(value * 2));
+
+      mappedMaybe.fold(
+        () => fail('Should return expected mapped value after flatmap'),
+        (value) => expect(value).toEqual(10)
+      );
+    });
+  });
+});
