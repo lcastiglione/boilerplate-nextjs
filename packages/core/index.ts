d7e3aa9129af7849ae770ab54855eb9b50adf83f
lcastiglione <leonardocastiglione@hotmail.com>
2023-03-02 12:00:43 -0300

    Add export Maybe class

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,4 +1,5 @@
-﻿export * from './src/domain/common/Either';
+﻿export * from './src/domain/common/Maybe';
+export * from './src/domain/common/Either';
 export * from './src/domain/common/EitherAsync';
 export * from './src/domain/common/ValueObject';
 export * from './src/presentation/common/Bloc';
