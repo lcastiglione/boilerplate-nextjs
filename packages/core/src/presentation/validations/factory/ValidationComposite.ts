7c4e6ff946cf055276fb28aeb5865de48a2408b9
lcastiglione <leonardocastiglione@hotmail.com>
2023-03-03 10:03:13 -0300

    Add ValidationComposite class

---
+++ packages/core/src/presentation/validations/factory/ValidationComposite.ts
@@ -0,0 +1,27 @@
+import { Either } from '@/domain/common/Either';
+import { FieldValidation } from '../common/FieldValidation';
+import { ValidationError } from '../common/ValidationError';
+
+export interface Validation {
+  validate: (fieldName: string, input: object) => Either<ValidationError[], boolean>;
+}
+
+export class ValidationComposite implements Validation {
+  private constructor(private readonly validators: FieldValidation[]) {}
+
+  static build(validators: FieldValidation[]): ValidationComposite {
+    return new ValidationComposite(validators);
+  }
+
+  validate(fieldName: string, input: object): Either<ValidationError[], boolean> {
+    const validators = this.validators.filter((v) => v.field === fieldName);
+    const errors: ValidationError[] = [];
+    validators.map((v) =>
+      v.validate(input).fold(
+        (error) => errors.push(error),
+        () => null
+      )
+    );
+    return errors.length > 0 ? Either.left(errors) : Either.right(true);
+  }
+}
