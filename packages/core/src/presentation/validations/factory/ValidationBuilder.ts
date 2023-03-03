1cc030014b9e35b211a4c8227c2f04cd89f1e76e
lcastiglione <leonardocastiglione@hotmail.com>
2023-03-03 10:02:52 -0300

    Add ValidationBuilder class

---
+++ packages/core/src/presentation/validations/factory/ValidationBuilder.ts
@@ -0,0 +1,38 @@
+import {
+  RequiredFieldValidation,
+  MinLengthValidation,
+  CompareFieldsValidation,
+} from '../validators';
+import { FieldValidation } from '../common/FieldValidation';
+
+export class ValidationBuilder {
+  private constructor(
+    private readonly fieldName: string,
+    private readonly validations: FieldValidation[]
+  ) {}
+
+  static field(fieldName: string): ValidationBuilder {
+    return new ValidationBuilder(fieldName, []);
+  }
+
+  required(): ValidationBuilder {
+    this.validations.push(new RequiredFieldValidation(this.fieldName));
+    return this;
+  }
+
+  min(length: number): ValidationBuilder {
+    this.validations.push(new MinLengthValidation(this.fieldName, length));
+    return this;
+  }
+
+  sameAs(fieldToCompare: string): ValidationBuilder {
+    this.validations.push(
+      new CompareFieldsValidation(this.fieldName, fieldToCompare)
+    );
+    return this;
+  }
+
+  build(): FieldValidation[] {
+    return this.validations;
+  }
+}
